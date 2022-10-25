623def099c8d2115727e9b505273be859c35a379
Pozdeeva.T <tanyajoooe@gmail.com>
2022-10-26 00:24:10 +0300

    added task_score.js

---
+++ task_score.js
@@ -0,0 +1,9 @@
+let student = "Tatyana Pozdeeva";
+let modIntro = 33;
+let modGit = 100;
+let modJs = 55;
+let averageValue = (modIntro + modGit + modJs) / 3;
+
+console.log(
+  `Средний балл по трём модулям студента ${student}: ` + averageValue
+);
