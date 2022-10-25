bbfcb229b71044f4f7dff18c947a3b3c95a6ec21
Pozdeeva.T <tanyajoooe@gmail.com>
2022-10-26 00:50:52 +0300

    adde variable firstLetterStudent1

--- task_score.js
+++ task_score.js
@@ -1,9 +1,16 @@
-let student = "Tatyana Pozdeeva";
+let student1 = "Tatyana Pozdeeva";
 let modIntro = 33;
 let modGit = 100;
 let modJs = 55;
 let averageValue = (modIntro + modGit + modJs) / 3;
+averageValue = Math.round(averageValue);
+
+//Метод slice вырезает и возвращает указанную часть массива.
+//Первым параметром указывается номер элемента массива, с которого начинается вырезание, а вторым параметром - номер элемента, на котором закончится вырезание (при этом элемент с этим номером не включится в вырезанную часть).
+
+let firstLetterStudent1 = student1.slice(0, 1);
 
 console.log(
-  `Средний балл по трём модулям студента ${student}: ` + averageValue
+  `Средний балл по трём модулям студента ${firstLetterStudent1}: ` +
+    averageValue
 );
