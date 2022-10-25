859f460e4e7e4e0463df270eb67823c58f90377b
Pozdeeva.T <tanyajoooe@gmail.com>
2022-10-26 00:55:07 +0300

    update task_score.js

--- task_score.js
+++ task_score.js
@@ -3,11 +3,10 @@ let modIntro = 33;
 let modGit = 100;
 let modJs = 55;
 let averageValue = (modIntro + modGit + modJs) / 3;
-averageValue = Math.round(averageValue);
+averageValue = Math.round(averageValue); //округдение до целых
 
 //Метод slice вырезает и возвращает указанную часть массива.
 //Первым параметром указывается номер элемента массива, с которого начинается вырезание, а вторым параметром - номер элемента, на котором закончится вырезание (при этом элемент с этим номером не включится в вырезанную часть).
-
 let firstLetterStudent1 = student1.slice(0, 1);
 
 console.log(
